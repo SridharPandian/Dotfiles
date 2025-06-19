07b485a27a3d910cdffeff3301d2a493e40f6e71
SridharPandian <sripan172@gmail.com>
2025-06-19 17:28:39 -0400

    Added automated dependency install.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,28 @@
 
 set -e
 
+# OS specific dependency install
+OS_TYPE="$(uname)"
+echo "Detected OS: $OS_TYPE"
+
+DEPENDENCIES=(curl tmux vim)
+
+if [ "$OS" = "Darwin" ]; then
+    for pkg in "${PKGS[@]}"; do
+        brew install $pkg
+    done
+elif [ "$OS" = "Linux" ]; then
+    sudo apt update -y
+
+    for pkg in "${PKGS[@]}"; do
+        sudo apt install -y $pkg
+    done
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+
 # Setup ohmyzsh
 sh -c "$(curl -fsSL https://install.ohmyz.sh)"
 
