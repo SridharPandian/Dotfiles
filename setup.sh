679d859e9d97dc0fb170423e47471037e56703cb
SridharPandian <sripan172@gmail.com>
2025-06-20 02:07:45 +0400

    Added tmux configs.

--- setup.sh
+++ setup.sh
@@ -23,11 +23,14 @@ else
     exit 1
 fi
 
-
 # Setup ohmyzsh
 sh -c "$(curl -fsSL https://install.ohmyz.sh)"
 
 # Copy the zsh config to the user home folder
 cp zsh/.zshrc $HOME/.zshrc
+
+# Copying the tmux config and activating the changes
+cp tmux/.tmux.conf $HOME/.tmux.conf
+tmux source-file ~/.tmux.conf
 
 set +e
